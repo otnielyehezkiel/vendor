f83e313d3332546d60c2e8d27e5afbda37211392
otnielyehezkiel <striketokill@gmail.com>
2016-03-14 01:59:17 +0700

    update

--- src/AppBundle/Controller/PerusahaanServiceController.php
+++ src/AppBundle/Controller/PerusahaanServiceController.php
@@ -5,28 +5,29 @@ namespace AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use NoiseLabs\Bundle\NuSOAPBundle;
 
 class PerusahaanServiceController extends Controller
 {
-	 public function indexAction(Request $request)
+    /**
+     * @Route("/perusahaan")
+     */
+	 public function indexAction()
     {
-        // replace this example code with whatever you need
-         $server = new \SoapServer('/path/to/hello.wsdl'); 
-
-        $server->setObject($this); 
-
-        $response = new Response(); 
-
-        $response->headers->set('Content-Type', 'text/xml; charset=ISO-8859-1'); 
-
-        ob_start(); 
-
-        $server->handle(); 
-
-        $response->setContent(ob_get_contents()); 
-
-        ob_clean(); 
-
-        return $response; 
+        $wsdl = "http://www.webservicex.net/geoipservice.asmx?WSDL";
+        $client = new \nusoap_client($wsdl,'wsdl');
+        $result = $client->call("GetGeoIP",array("IPAddress"=> "202.46.129.20"));
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
+        $product = $this->getDoctrine()
+        ->getRepository('AppBundle:Perusahaan')
+        ->find("147");
+        echo "<pre>";
+        print_r($product);
+        echo "</pre>";
+        return $product;
+        //print_r($product);
     }
 }
--- web/app.php
+++ web/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Debug\Debug;
 
 /**
  * @var Composer\Autoload\ClassLoader
